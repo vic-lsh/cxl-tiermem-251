dac7ef585fbbe42d1f09f98245211ecd3cd55947
Matthew Giordano <mgiordan@cs.washington.edu>
2025-06-05 10:38:19 -0700

    try adding ~empty to rdreq and ~full to wrreq (shouldn't make a difference because overflow_checking is enabled)

--- intel_rtile_cxl_top_0_ed/hardware_test_design/common/afu/afu_top.sv
+++ intel_rtile_cxl_top_0_ed/hardware_test_design/common/afu/afu_top.sv
@@ -972,11 +972,11 @@ sync_fifo #(
 	.clk(clk),
     .rst(~rst_n),
 
-    .wrreq(ip2hdm_aximm0_arvalid),
+    .wrreq(ip2hdm_aximm0_arvalid && ~fifo_full[0]),
     .data({ip2hdm_aximm0_arid, ip2hdm_aximm0_araddr, ip2hdm_aximm0_arlen, ip2hdm_aximm0_arregion, ip2hdm_aximm0_aruser, ip2hdm_aximm0_arsize, ip2hdm_aximm0_arburst, ip2hdm_aximm0_arprot, ip2hdm_aximm0_arqos, ip2hdm_aximm0_arcache, ip2hdm_aximm0_arlock}),
     .full(fifo_full[0]),
 
-    .rdreq(hdm2ip_aximm_arready[0]),
+    .rdreq(hdm2ip_aximm_arready[0] && ~fifo_empty[0]),
     .q({ip2hdm_aximm_arid[0], ip2hdm_aximm_araddr[0], ip2hdm_aximm_arlen[0], ip2hdm_aximm_arregion[0], ip2hdm_aximm_aruser[0], ip2hdm_aximm_arsize[0], ip2hdm_aximm_arburst[0], ip2hdm_aximm_arprot[0], ip2hdm_aximm_arqos[0], ip2hdm_aximm_arcache[0], ip2hdm_aximm_arlock[0]}),
     .empty(fifo_empty[0])
 );
@@ -989,11 +989,11 @@ sync_fifo #(
 	.clk(clk),
     .rst(~rst_n),
 
-    .wrreq(ip2hdm_aximm1_arvalid),
+    .wrreq(ip2hdm_aximm1_arvalid && ~fifo_full[1]),
     .data({ip2hdm_aximm1_arid, ip2hdm_aximm1_araddr, ip2hdm_aximm1_arlen, ip2hdm_aximm1_arregion, ip2hdm_aximm1_aruser, ip2hdm_aximm1_arsize, ip2hdm_aximm1_arburst, ip2hdm_aximm1_arprot, ip2hdm_aximm1_arqos, ip2hdm_aximm1_arcache, ip2hdm_aximm1_arlock}),
     .full(fifo_full[1]),
 
-    .rdreq(hdm2ip_aximm_arready[1]),
+    .rdreq(hdm2ip_aximm_arready[1] && ~fifo_empty[1]),
     .q({ip2hdm_aximm_arid[1], ip2hdm_aximm_araddr[1], ip2hdm_aximm_arlen[1], ip2hdm_aximm_arregion[1], ip2hdm_aximm_aruser[1], ip2hdm_aximm_arsize[1], ip2hdm_aximm_arburst[1], ip2hdm_aximm_arprot[1], ip2hdm_aximm_arqos[1], ip2hdm_aximm_arcache[1], ip2hdm_aximm_arlock[1]}),
     .empty(fifo_empty[1])
 );
@@ -1006,11 +1006,11 @@ SoftFIFO #(
 	.clock(clk),
     .reset_n(rst_n),
 
-    .wrreq(ip2hdm_aximm0_arvalid),
+    .wrreq(ip2hdm_aximm0_arvalid && ~fifo_full[0]),
     .data({ip2hdm_aximm0_arid, ip2hdm_aximm0_araddr, ip2hdm_aximm0_arlen, ip2hdm_aximm0_arregion, ip2hdm_aximm0_aruser, ip2hdm_aximm0_arsize, ip2hdm_aximm0_arburst, ip2hdm_aximm0_arprot, ip2hdm_aximm0_arqos, ip2hdm_aximm0_arcache, ip2hdm_aximm0_arlock}),
     .full(fifo_full[0]),
 
-    .rdreq(hdm2ip_aximm_arready[0]),
+    .rdreq(hdm2ip_aximm_arready[0] && ~fifo_empty[0]),
     .q({ip2hdm_aximm_arid[0], ip2hdm_aximm_araddr[0], ip2hdm_aximm_arlen[0], ip2hdm_aximm_arregion[0], ip2hdm_aximm_aruser[0], ip2hdm_aximm_arsize[0], ip2hdm_aximm_arburst[0], ip2hdm_aximm_arprot[0], ip2hdm_aximm_arqos[0], ip2hdm_aximm_arcache[0], ip2hdm_aximm_arlock[0]}),
     .empty(fifo_empty[0])
 );
@@ -1022,11 +1022,11 @@ SoftFIFO #(
 	.clock(clk),
     .reset_n(rst_n),
 
-    .wrreq(ip2hdm_aximm1_arvalid),
+    .wrreq(ip2hdm_aximm1_arvalid && ~fifo_full[1]),
     .data({ip2hdm_aximm1_arid, ip2hdm_aximm1_araddr, ip2hdm_aximm1_arlen, ip2hdm_aximm1_arregion, ip2hdm_aximm1_aruser, ip2hdm_aximm1_arsize, ip2hdm_aximm1_arburst, ip2hdm_aximm1_arprot, ip2hdm_aximm1_arqos, ip2hdm_aximm1_arcache, ip2hdm_aximm1_arlock}),
     .full(fifo_full[1]),
 
-    .rdreq(hdm2ip_aximm_arready[1]),
+    .rdreq(hdm2ip_aximm_arready[1] && ~fifo_empty[1]),
     .q({ip2hdm_aximm_arid[1], ip2hdm_aximm_araddr[1], ip2hdm_aximm_arlen[1], ip2hdm_aximm_arregion[1], ip2hdm_aximm_aruser[1], ip2hdm_aximm_arsize[1], ip2hdm_aximm_arburst[1], ip2hdm_aximm_arprot[1], ip2hdm_aximm_arqos[1], ip2hdm_aximm_arcache[1], ip2hdm_aximm_arlock[1]}),
     .empty(fifo_empty[1])
 );
